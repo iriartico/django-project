47275fc402b1c07958802bbb164984056154eb38
Steve Jobs <sexyespadachin1@gmail.com>
2024-10-31 01:05:03 -0400

    First configs completed

--- myapp/views.py
+++ myapp/views.py
@@ -4,18 +4,27 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    tittle = "Django Course!!"
+    return render(request, 'index.html', {
+        'tittle': tittle
+    })
 
 def about(request):
     return render(request, 'about.html')
 
 def projects(request):
-    projects = list(Project.objects.values())
+    # projects = list(Project.objects.values())
+    projects = list(Project.objects.all())
     # return JsonResponse(projects, safe=False)
-    return render(request, 'projects.html')
+    return render(request, 'projects.html', {
+        'projects': projects
+    })
 
 def tasks(request):
     # task = Task.objects.get(id=id)
     # task = get_object_or_404(Task, id=id)
     # return HttpResponse('Task: %s' % task.title)
-    return render(request, 'tasks.html')
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
